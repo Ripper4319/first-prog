16a15d0dc7cee5840a39f4bcec9efedf69626fd7
Ripper4319 <filmguy4319@gmail.com>
2024-10-04 23:08:55 -0700

    hbjn

--- First/Assets/Script/crosshair scripts.cs
+++ First/Assets/Script/crosshair scripts.cs
@@ -3,31 +3,33 @@ using UnityEngine.UI;
 
 public class CrosshairController : MonoBehaviour
 {
-    public Image crosshair; 
-    public Transform gunslot; 
-    public Camera mainCamera; 
-
-    public float maxRaycastDistance = 100f; 
+    public Image crosshair;
+    public Transform gunslot;
+    public Camera mainCamera;
+    public float interactDistance = 5f;
+    public LayerMask interactableitems;
+    public Text objectname;
+    public float maxRaycastDistance = 100;
 
     void Update()
     {
-        
         Ray ray = new Ray(gunslot.position, -gunslot.up);
         RaycastHit hit;
 
-        
-        if (Physics.Raycast(ray, out hit, maxRaycastDistance))
+        if (Physics.Raycast(ray, out hit, maxRaycastDistance, interactableLayer))
         {
-            
             Vector3 screenPoint = mainCamera.WorldToScreenPoint(hit.point);
-
-            
             crosshair.transform.position = screenPoint;
+
+            if (hit.distance <= interactDistance)
+            {
+                objectname.text = hit.collider.gameObject.name;
+            }
         }
         else
         {
-            
             crosshair.transform.position = new Vector3(Screen.width / 2, Screen.height / 2);
+            objectname.text = "";
         }
     }
 }
